3d2049559edd0d27353b3d523fa13918e6015726
Ali Aghaei Saem <a.aghaei.s@gmail.com>
2019-11-23 23:15:53 +0330

    Honors have been changed

--- src/data/resume/honor.js
+++ src/data/resume/honor.js
@@ -1,31 +1,31 @@
 const honor = [
   {
+    company: "Electrical and Computer Engineering Department, University of Tehran",
+    position: "Iran",
+    link: "#",
+    daterange: "2015 - 2019",
+    points: ["Ranked 1st among 128 students"]
+  },
+  {
+    company: "National Electrical Engineering Olympiad",
+    position: "Iran",
+    link: "#",
+    daterange: "2018",
+    points: ["Ranked 8th among best nationwide electrical engineering students"]
+  },
+  {
     company: "National Elites Foundation Grant",
     position: "Iran",
     link: "#",
-    daterange: "2017 - 2018",
+    daterange: "2017 - Present",
     points: ["Outstanding academic performance"]
-  },
-  {
-    company: "Support Foundation of University of Tehran Grant",
-    position: "Iran",
-    link: "#",
-    daterange: "2017 - 2018",
-    points: ["Outstanding academic performance"]
-  },
-  {
-    company: "DAAD Scholarship",
-    position: "Germany",
-    link: "#",
-    daterange: "2018",
-    points: ["Scholarship for internship by the German Academic Exchange Services"]
   },
   {
     company: "Nationwide University Entrance Exam",
     position: "Iran",
     link: "#",
     daterange: "2015",
-    points: ["Ranked 237th place among all applicants in the physics and mathematics exam (approximately 182,000 applicants)","21st among 7,000 participants in the foreign languages exam"]
+    points: ["Ranked 171th place among all applicants in the physics and mathematics exam (approximately 182,000 applicants)","21st among 7,000 participants in the foreign languages exam"]
   }
 ];
 
