364b0d36a11e078a1d7f02093d13bbfbf1b554c4
Ali Ranjbar <ali1377ranjbar@gmail.com>
2019-12-04 22:42:04 +0330

    remove jangolaks

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -7,75 +7,7 @@ import Main from "../layouts/Main";
 
 import data from "../data/contact";
 
-const validateText = text => {
-  const re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))$/;
-  return re.test(text) || text.length === 0;
-};
-
-const messages = [
-  "hi",
-  "hello",
-  "hola",
-  "you-can-email-me-at-literally-anything! Really",
-  "well, not anything. But most things",
-  "like-this",
-  "or-this",
-  "but not this :(  ",
-  "you.can.also.email.me.with.specific.topics.like",
-  "just-saying-hi",
-  "please-work-for-us",
-  "help",
-  "admin",
-  "or-I-really-like-your-website",
-  "I'll-stop-distracting-you-now",
-  "thanks"
-];
-
-const useInterval = (callback, delay) => {
-  const savedCallback = useRef();
-  useEffect(() => {
-    savedCallback.current = callback;
-  }, [callback]);
-
-  useEffect(() => {
-    if (delay) {
-      const id = setInterval(() => {
-        savedCallback.current();
-      }, delay);
-      return () => clearInterval(id);
-    }
-    return () => {}; // pass linter
-  }, [delay]);
-};
-
 const Contact = () => {
-  const hold = 50; // ticks to wait after message is complete before rendering next message
-  const delay = 50; // tick length in mS
-
-  const [idx, updateIter] = useState(0); // points to current message
-  const [message, updateMessage] = useState(messages[idx]);
-  const [char, updateChar] = useState(messages[idx].length); // points to current char
-  const [isActive, setIsActive] = useState(true); // disable when all messages are printed
-
-  useInterval(
-    () => {
-      let newIdx = idx;
-      let newChar = char;
-      if (char - hold >= messages[idx].length) {
-        newIdx += 1;
-        newChar = 0;
-      }
-      if (newIdx === messages.length) {
-        setIsActive(false);
-      } else {
-        updateMessage(messages[newIdx].slice(0, newChar));
-        updateIter(newIdx);
-        updateChar(newChar + 1);
-      }
-    },
-    isActive ? delay : null
-  );
-
   return (
     <Main>
       <Helmet title="Contact" />
@@ -89,20 +21,14 @@ const Contact = () => {
         </header>
         <div className="email-at">
           <p>Feel free to get in touch. You can email me at: </p>
-          <div
-            className="inline-container"
-            style={validateText(message) ? {} : { color: "red" }}
-            onMouseEnter={() => setIsActive(false)}
-            onMouseLeave={() => idx < messages.length && setIsActive(true)}
-          >
-            <a
-              href={
-                validateText(message) ? `mailto:a.aghaei.s@gmail.com` : ""
-              }
-            >
-              <span>{message}</span>
-              <span>@aranjbar.me</span>
-            </a>
+          <div className="inline-container">
+            <span>a.aghaei.s{"{at-sign}"}gmail.com</span>
+          </div>
+          <div className="inline-container">
+            <span>a.aghaei.s{"{at-sign}"}yahoo.com</span>
+          </div>
+          <div className="inline-container">
+            <span>a.aghaei.s{"{at-sign}"}yahoo.com</span>
           </div>
         </div>
         <ul className="icons">
